73df8547d459ffcb5570672d12a7276469c033cc
Carlos Morera de la Chica <carlos@codurance.com>
2017-01-14 16:48:57 +0000

    Update Ruby in PATH

--- paths.sh
+++ paths.sh
@@ -2,9 +2,4 @@ export PATH=${PATH}:~/Android/Sdk/tools/
 export PATH=${PATH}:~/Android/Sdk/platform-tools/
 export PATH=${PATH}:/opt/pypy3/bin/
 export PATH=${PATH}:~/.bin/
-
-#Ruby
-GEM_HOME=$(ls -t -U | ruby -e 'puts Gem.user_dir')
-GEM_PATH=$GEM_HOME  
-export PATH=$PATH:$GEM_HOME/bin
-export PATH=${PATH}:"$(ruby -e 'print Gem.user_dir')/bin"
+export PATH=$PATH:$HOME/.rvm/bin
