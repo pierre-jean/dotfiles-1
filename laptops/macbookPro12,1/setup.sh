4c4b9c9730b9af7f25e7e9485cc7bcb4c3f7c285
Carlos Morera de la Chica <carlos@codurance.com>
2017-01-15 13:24:02 +0000

    Remove not needed permissions granting

--- laptops/macbookPro12,1/setup.sh
+++ laptops/macbookPro12,1/setup.sh
@@ -14,7 +14,6 @@ sudo cp 90-backlight.rules /etc/udev/rules.d/90-backlight.rules
 
 #Fixes dpi of i3
 bash -c 'echo -e "xrandr --dpi 160\nxrdb -merge ~/.Xresources" >> ~/.before_startx/run.sh'
-chmod a+x ~/.before_startx/run.sh
 bash -c 'echo -e "Xft.dpi: 160.0" >> ~/.Xresources'
 
 #Fns instead of media keys
