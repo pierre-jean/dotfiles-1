54d2a542f18b897b5859df4c03fd254f2d3a5a2a
Carlos Morera de la Chica <carlos@codurance.com>
2017-01-27 12:16:12 +0000

    Add gitignore global files for vim, linux, windows, etc...

--- setup/setup.sh
+++ setup/setup.sh
@@ -96,8 +96,9 @@ installClojure() {
 
 installGit() {
   echo "Installing Git"
-  yaourt --noconfirm -S diff-so-fancy
+  yaourt --noconfirm -S ./yaourt_git.txt
   ln -sfn ${dir}/.gitconfig ${HOME}/.gitconfig
+  gibo Vim JetBrains Tags Vagrant Windows macOS Linux Archives >> ~/.gitignore.global
 }
 
 installDocker() {
