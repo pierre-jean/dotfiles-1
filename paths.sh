f4b6b16764e7b7881e6517b4abe872787b1b81e3
Carlos Morera de la Chica <carlos@codurance.com>
2017-02-03 19:56:44 +0000

    Remove wrong directory from path

--- paths.sh
+++ paths.sh
@@ -2,6 +2,5 @@ export PATH=${PATH}:~/Android/Sdk/tools/
 export PATH=${PATH}:~/Android/Sdk/platform-tools/
 export PATH=${PATH}:/opt/pypy3/bin/
 export PATH=${PATH}:~/.bin/
-export PATH=$PATH:$HOME/.rvm/bin
 export PATH=$PATH:$HOME/.local/bin/
 export PATH=${PATH}:/usr/bin/terragrunt
