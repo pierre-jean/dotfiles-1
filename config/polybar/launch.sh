de566cc9fa21b07edc23273d4ca70038cbdc6790
Carlos Morera de la Chica <carlos@codurance.com>
2017-07-01 12:07:51 +0100

    Update monitors names in polybar config

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -7,8 +7,8 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # External monitor bar
-POLYBAR_MONITOR=DP-1 polybar top &
-POLYBAR_MONITOR=HDMI-2 polybar top &
+POLYBAR_MONITOR=DP1 polybar top &
+POLYBAR_MONITOR=HDMI2 polybar top &
 
 # MacBook Pro bar
 POLYBAR_MONITOR=eDP-1 POLYBAR_DPI=120 polybar top &
