235b926771fff391e6e7c68fdff7b35b78d3fbde
Carlos Morera de la Chica <carlos@codurance.com>
2017-09-20 23:03:44 +0100

    Restart i3 when changing monitors configuration

--- bin/home-layout.sh
+++ bin/home-layout.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 xrandr --output DP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --mode 1600x1200 --pos 1920x0 --rotate left --output DP2 --off
+./i3-restart.sh
 variety --next
+
--- bin/home-with-tv-layout.sh
+++ bin/home-with-tv-layout.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 xrandr --output DP2 --mode 1360x768 --pos 3120x0 --rotate normal --output DP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI2 --mode 1600x1200 --pos 1920x0 --rotate left
+./i3-restart.sh
 variety --next
