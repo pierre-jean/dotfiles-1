4c8b0ef779db5c9e541f18b4bf58a80ea4a2cd13
Carlos Morera de la Chica <carlos@codurance.com>
2017-09-23 00:13:27 +0100

    Add bindings to move windows in emacs with C-hjkl

--- config/emacs/lisp/vim-mode.el
+++ config/emacs/lisp/vim-mode.el
@@ -16,6 +16,12 @@
   (define-key evil-motion-state-map (kbd "C-w <left>") 'evil-window-left)
   (define-key evil-motion-state-map (kbd "C-w <right>") 'evil-window-right)
 
+  ;; Window navigation
+  (define-key evil-motion-state-map (kbd "C-k") 'evil-window-up)
+  (define-key evil-motion-state-map (kbd "C-j") 'evil-window-down)
+  (define-key evil-motion-state-map (kbd "C-h") 'evil-window-left)
+  (define-key evil-motion-state-map (kbd "C-l") 'evil-window-right)
+
   ;; Buffer navigation
   (define-key evil-motion-state-map (kbd "[ b") 'evil-prev-buffer)
   (define-key evil-motion-state-map (kbd "] b") 'evil-next-buffer)
