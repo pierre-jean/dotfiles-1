df1b4dad1e04aac7c152114e8c32530c4c7ecc56
Christian Panadero <panavtec@gmail.com>
2017-09-24 11:14:47 +0100

    Get rid of commentary warning in elisp

--- config/emacs/lisp/el.el
+++ config/emacs/lisp/el.el
@@ -3,5 +3,7 @@
 ;;; Code:
 (add-hook 'emacs-lisp-mode-hook 'programming-mode)
 (add-hook 'emacs-lisp-mode-hook 'company-mode)
+(add-hook 'flycheck-mode-hook
+          (lambda () (setq-default flycheck-disabled-checkers '(emacs-lisp-checkdoc))))
 
 (provide 'el)
