7f2fd4c5294c5e4d261076c4b296f16d48356195
Christian Panadero <panavtec@gmail.com>
2018-01-24 09:52:54 +0000

    Fix vim plugins loading

--- config/emacs/lisp/vim-mode.el
+++ config/emacs/lisp/vim-mode.el
@@ -70,14 +70,12 @@
 (pkg
   evil-surround
   :ensure t
-  :defer t
   :config
   (global-evil-surround-mode))
 
 (pkg
   evil-leader
   :ensure t
-  :defer t
   :config
   (global-evil-leader-mode)
   (evil-leader/set-leader ","))
@@ -85,7 +83,6 @@
 (pkg
   evil-numbers
   :ensure t
-  :defer t
   :config
 
   (define-key evil-normal-state-map (kbd "C-a") 'evil-numbers/inc-at-pt)
