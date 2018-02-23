bda627a900b7b3da8cd781dee47b6a83ea69ee4e
Christian Panadero <panavtec@gmail.com>
2018-02-23 13:37:20 +0000

    eEeEeEeEeEeEehhhhhhhh

--- config/emacs/lisp/vim-mode.el
+++ config/emacs/lisp/vim-mode.el
@@ -65,7 +65,8 @@
   (evil-ex-define-cmd "W" "w")
   (evil-ex-define-cmd "Wa" 'evil-write-all)
   (evil-ex-define-cmd "Q" "q")
-  (evil-ex-define-cmd "Qa" "qa"))
+  (evil-ex-define-cmd "Qa" "qa")
+  (evil-ex-define-cmd "E" "e"))
 
 (use-package
   evil-surround
