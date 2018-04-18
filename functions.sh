9c6f95838815fc8f43fd2ec95ccf7b0c2d36fb3f
Carlos Morera de la Chica <carlos@codurance.com>
2018-04-18 22:33:09 +0100

    Fix broken monitors script

--- functions.sh
+++ functions.sh
@@ -241,7 +241,8 @@ soundTvOutput() {
 
 screenHomeLayout() {
   xrandr --output DP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
-         --output HDMI2 --mode 1600x1200 --pos 1920x0 --rotate left
+         --output HDMI2 --mode 1600x1200 --pos 1920x0 --rotate left \
+         --output DP2 --off
   variety --next
   soundMonitorOutput
 }
