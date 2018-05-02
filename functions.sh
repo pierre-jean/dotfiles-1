2ac97afbff6aaf425267d3bbdeea82aded34f289
Carlos Morera de la Chica <carlos@codurance.com>
2018-05-02 15:12:24 +0100

    Add screen function for only macbook

--- functions.sh
+++ functions.sh
@@ -255,6 +255,15 @@ screenHomeWithTvLayout() {
   soundTvOutput
 }
 
+screenMacbookLayout() {
+  xrandr --output eDP1 --primary --mode 2560x1600 --pos 0x0 --rotate normal \
+    --output VIRTUAL1 --off \
+    --output DP1 --off \
+    --output HDMI2 --off \
+    --output HDMI1 --off \
+    --output DP2 --off
+}
+
 screenMacbookRightLayout() {
   xrandr --output eDP1 --mode 2560x1600 --pos 2560x0 --rotate normal \
          --output DP2 --primary --mode 2560x1440 --pos 0x80 --rotate normal
