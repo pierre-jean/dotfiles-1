4c5ef0d2f162972464f2e2ff7fa5a3ea3da53621
Carlos Morera de la Chica <carlos@codurance.com>
2018-06-18 13:27:12 +0100

    Update screen home layout

--- functions.sh
+++ functions.sh
@@ -240,9 +240,12 @@ soundTvOutput() {
 }
 
 screenHomeLayout() {
-  xrandr --output DP1 --primary --mode 1920x1080 --pos 0x0 --rotate normal \
-         --output HDMI2 --mode 1600x1200 --pos 1920x0 --rotate left \
-         --output DP2 --off
+  xrandr --output DP2 --mode 1920x1080 --pos 0x0 --rotate normal \
+         --output DP1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal \
+         --output HDMI2 --mode 1600x1200 --pos 3840x0 --rotate left \
+         --output HDMI1 --off \
+         --output VIRTUAL1 --off \
+         --output DP3 --off
   variety --next
   soundMonitorOutput
 }
